a0c1b44e44490d6314839bc533c803355182884a
Colleen Woolsey <rlwool@aol.com>
2019-01-25 12:12:06 -0600

    Ch 6 - Using modules in React

---
+++ src/modules/AnimalManager.js
@@ -0,0 +1,24 @@
+const remoteURL = "http://localhost:5002"
+
+export default {
+  get(id) {
+    return fetch(`${remoteURL}/animals/${id}`)
+    .then(e => e.json())
+  },
+  getAll() {
+    return fetch(`${remoteURL}/animals`)
+    .then(e => e.json())
+  },
+   deleteAnimal(id) {
+        return fetch(`http://localhost:5002/animals/${id}`, {
+            method: "DELETE"
+        })
+        .then(e => e.json())
+        .then(() => fetch(`http://localhost:5002/animals`))
+        .then(e => e.json())
+        .then(animals => this.setState({
+            animals: animals
+        })
+      )
+    }
+}
